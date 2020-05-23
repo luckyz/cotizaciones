a11b128037b153a2ac7b5fe003d410852df96e7b
Lucas Zurverra <lzurverra@gmail.com>
2020-05-23 11:04:58 -0300

    agregado soporte para dolariol y bna

---
+++ main.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+import requests
+from bs4 import BeautifulSoup
+
+urls = {
+    "iol": {
+        "url": "https://dolariol.com/",
+        "compra": ".precio-compra",
+        "venta": ".precio-venta"
+    },
+    "bna": {
+        "url": "https://bna.com.ar/Personas",
+        "compra": "#billetes > table > tbody > tr:nth-child(1) > td:nth-child(2)",
+        "venta": "#billetes > table > tbody > tr:nth-child(1) > td:nth-child(3)"
+    }
+}
+
+
+
+class Cotizacion(object):
+    def __init__(self, id):
+        super(Cotizacion, self).__init__()
+        self.id = id
+        self.url = urls[self.id]["url"]
+        self.soup = None
+
+    def obtener(self):
+        return requests.get(self.url).content
+
+    def imprimir(self):
+        self.soup = BeautifulSoup(self.obtener(), "html.parser")
+        return self.soup
+
+    def compra(self):
+        return urls[self.id]["compra"]
+
+    def venta(self):
+        return urls[self.id]["venta"]
+
+    def cotizacion(self, selector):
+        row = self.soup.select_one(selector)
+        precio = "${:.2f}".format(float((row.text.replace(",", "."))))
+
+        return precio.replace(".", ",")
+
+
+def main():
+    # ejemplo
+    c = Cotizacion("bna")
+    print(c.url)
+    c.imprimir()
+    print(c.cotizacion(c.compra()))
+
+if __name__ == '__main__':
+    main()

e5c35f33dc21b5afa66b1312bb2ae6406119ba7d
Lucas Zurverra <lzurverra@gmail.com>
2020-05-25 19:07:59 -0300

    agregado uso de * para ver todas las cotizaciones

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cotizaciones
 Obtener y cotizar acciones segun una moneda especifica
+
+Abrir una terminal y ejecutar:
+``python3 main.py```
+
+Utilizar * para consultar por todas 
--- main.py
+++ main.py
@@ -18,29 +18,60 @@ urls = {
 
 
 class Cotizacion(object):
-    def __init__(self, id):
+    def __init__(self, entidad=None):
         super(Cotizacion, self).__init__()
-        self.id = id
-        self.url = urls[self.id]["url"]
-        self.soup = BeautifulSoup(requests.get(self.url).content, "html.parser")
+        self.entidad = entidad
+        if self.entidad != None:
+            self.url = urls[self.id]["url"]
+            self.soup = BeautifulSoup(requests.get(self.url).content, "html.parser")
+        else:
+            self.url = None
+            self.soup = None
+        self.urls = urls
 
     def transformar(self, valor):
         return "${:.2f}".format(float((valor.text.replace(",", ".")))).replace(".", ",")
 
-    def cotizacion_compra(self):
-        row = self.soup.select_one(urls[self.id]["compra"])
+    def cotizacion_compra(self, entidad):
+        self.url = urls[entidad]["url"]
+        self.soup = BeautifulSoup(requests.get(self.url).content, "html.parser")
+        row = self.soup.select_one(urls[entidad]["compra"])
         return self.transformar(row)
 
-    def cotizacion_venta(self):
-        row = self.soup.select_one(urls[self.id]["venta"])
+    def cotizacion_venta(self, entidad):
+        self.url = urls[entidad]["url"]
+        self.soup = BeautifulSoup(requests.get(self.url).content, "html.parser")
+        row = self.soup.select_one(urls[entidad]["venta"])
         return self.transformar(row)
+
+    def cotizar(self):
+        e, c, v = [], [], []
+        for entidad in urls.keys():
+            e.append("=== {} ===".format(entidad.upper()))
+            c.append("Compra: {}".format(self.cotizacion_compra(entidad)))
+            v.append("Venta: {}".format(self.cotizacion_venta(entidad)))
+
+        return e, c, v
 
 
 def main():
     # ejemplo
-    c = Cotizacion("bna")
-    print(c.url)
-    print(c.cotizacion_compra())
+    c = Cotizacion()
+    print("Entidades soportadas: {}".format(", ".join([x for x in urls.keys()])))
+    # c.cotizar()
+    entidad = str(input(">> Ingrese el nombre de una entidad para consultar: " ))
+    if entidad != "*":
+        compra = c.cotizacion_compra(entidad)
+        venta = c.cotizacion_venta(entidad)
+        print("=== {} ===".format(entidad.upper()))
+        print("Compra: {}".format(compra))
+        print("Venta: {}".format(venta))
+    else:
+        for entidad in urls:
+            print("=== {} ===".format(entidad.upper()))
+            print(c.cotizacion_compra(entidad))
+            print(c.cotizacion_venta(entidad))
+            print("")
 
 if __name__ == '__main__':
     main()
